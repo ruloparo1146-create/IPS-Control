14c71af72e1927086c66c96bb11e945c53bdef0b
ruloparo1146-create <ruloparo1146@gmail.com>
2026-09-21 15:48:43 +0200

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,18 +1,14 @@
 // js/auth.js
-
-// FunciÃ³n para iniciar sesiÃ³n
 async function iniciarSesion(email, password) {
   const { data, error } = await window.db.auth.signInWithPassword({ email, password });
   return { data, error };
 }
 
-// FunciÃ³n para cerrar sesiÃ³n
 async function cerrarSesion() {
   await window.db.auth.signOut();
   window.location.href = 'login.html';
 }
 
-// FunciÃ³n para verificar si hay sesiÃ³n activa
 async function verificarSesion() {
   const { data: { session } } = await window.db.auth.getSession();
   return session;
