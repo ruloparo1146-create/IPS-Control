18db5bdc30d0188b48ee2304e1ab5bdf3aedbfec
ruloparo1146-create <ruloparo1146@gmail.com>
2026-09-21 15:50:05 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 // js/config.js
-const SUPABASE_URL = 'https://mlyswiebbatzllqlrctw.supabase.co';
+const SUPABASE_URL = 'https://mlyswiebbatzllqrctw.supabase.co';
 const SUPABASE_KEY = 'sb_publishable_IQ8Ckee41etEJRT_c8weCg_YSEcR2sp';
